1e9b60a93322b9685950c1cbffe844b8277d2771
MagiFeeney <MagiFeeney>
2022-08-19 00:05:41 +0800

    update script

--- run_all.sh
+++ run_all.sh
@@ -9,19 +9,19 @@ do
         python main.py --env-name "${env[$index]}-v3" --algo ppo --clip-param 0.2 --use-gae --log-dir "logs/${env[$index]}/PPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --use-linear-lr-decay --use-proper-time-limits	
 
 	# PPO: Reward shaping with entropy
-        python main.py --env-name "${env[$index]}-v3" --algo ppo --alpha 0.01 --clip-param 0.2 --use-gae --log-dir "logs/${env[$index]}/SPPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --use-linear-lr-decay --use-proper-time-limits --augment-type "shifted" --temperature-decay-rate 0.93
+        python main.py --env-name "${env[$index]}-v3" --algo ppo --alpha 0.01 --clip-param 0.2 --use-gae --log-dir "logs/${env[$index]}/SPPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --use-linear-lr-decay --use-proper-time-limits --augment-type "shifted" --temperature-decay-rate 0.93 --epochs-drop 10
 
 	# PPO: Bootstrapping with state value function
-        python main.py --env-name "${env[$index]}-v3" --algo ppo --alpha 0.01 --clip-param 0.2 --use-gae --log-dir "logs/${env[$index]}/BPPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --use-linear-lr-decay --use-proper-time-limits --augment-type "bootstrap" --temperature-decay-rate 0.93
+        python main.py --env-name "${env[$index]}-v3" --algo ppo --alpha 0.01 --clip-param 0.2 --use-gae --log-dir "logs/${env[$index]}/BPPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --use-linear-lr-decay --use-proper-time-limits --augment-type "bootstrap" --temperature-decay-rate 0.93 --epochs-drop 10
 
 	# TRPO: Baseline
-	python main.py --env-name "${env[$index]}-v3" --algo trpo --use-gae --log-dir "logs/${env[$index]}/TRPO-${i}" --seed $i --log-interval 1 --num-steps 4096 --num-processes 1 --ppo-epoch 1 --num-mini-batch 1 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --max-kl 0.1 --damping 1e-1 --l2-reg 1e-3
+	python main.py --env-name "${env[$index]}-v3" --algo trpo --use-gae --log-dir "logs/${env[$index]}/TRPO-${i}" --seed $i --log-interval 1 --num-steps 4096 --num-processes 1 --ppo-epoch 1 --num-mini-batch 1 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --max-kl 0.1 --damping 1e-1 --l2-reg 1e-3 
 
 	# TRPO: Reward shaping with entropy
-	python main.py --env-name "${env[$index]}-v3" --algo trpo --use-gae --alpha 0.01 --log-dir "logs/${env[$index]}/STRPO-${i}" --seed $i --log-interval 1 --num-steps 4096 --num-processes 1 --ppo-epoch 1 --num-mini-batch 1 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --max-kl 0.1 --damping 1e-1 --l2-reg 1e-3 --augment-type "shifted" --temperature-decay-rate 0.93
+	python main.py --env-name "${env[$index]}-v3" --algo trpo --use-gae --alpha 0.01 --log-dir "logs/${env[$index]}/STRPO-${i}" --seed $i --log-interval 1 --num-steps 4096 --num-processes 1 --ppo-epoch 1 --num-mini-batch 1 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --max-kl 0.1 --damping 1e-1 --l2-reg 1e-3 --augment-type "shifted" --temperature-decay-rate 0.93 --epochs-drop 10
 	
 	# TRPO: Bootstrapping with state value function
-	python main.py --env-name "${env[$index]}-v3" --algo trpo --use-gae --alpha 0.01 --log-dir "logs/${env[$index]}/BTRPO-${i}" --seed $i --log-interval 1 --num-steps 4096 --num-processes 1 --ppo-epoch 1 --num-mini-batch 1 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --max-kl 0.1 --damping 1e-1 --l2-reg 1e-3 --augment-type "bootstrap" --temperature-decay-rate 0.93
+	python main.py --env-name "${env[$index]}-v3" --algo trpo --use-gae --alpha 0.01 --log-dir "logs/${env[$index]}/BTRPO-${i}" --seed $i --log-interval 1 --num-steps 4096 --num-processes 1 --ppo-epoch 1 --num-mini-batch 1 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 1000000 --max-kl 0.1 --damping 1e-1 --l2-reg 1e-3 --augment-type "bootstrap" --temperature-decay-rate 0.93 --epochs-drop 10
 
     done
 done
--- run_gridworld.sh
+++ run_gridworld.sh
@@ -6,13 +6,13 @@ do
     
     python test.py --env-name "TwoColors-v1" --algo ppo --clip-param 0.2 --use-gae --log-dir "logs_gridworld/TwoColors/PPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 250000 --use-linear-lr-decay --use-proper-time-limits
 
-    python test.py --env-name "TwoColors-v1" --algo ppo --alpha 0.01 --clip-param 0.2 --use-gae --log-dir "logs_gridworld/TwoColors/SPPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 250000 --use-linear-lr-decay --use-proper-time-limits --augment-type "shifted" --temperature-decay-rate 0.93
+    python test.py --env-name "TwoColors-v1" --algo ppo --alpha 0.01 --clip-param 0.2 --use-gae --log-dir "logs_gridworld/TwoColors/SPPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 250000 --use-linear-lr-decay --use-proper-time-limits --augment-type "shifted" --temperature-decay-rate 0.93 --epochs-drop 10
 
     # Diagonal Environment
     
     python test.py --env-name "Diagonal-v1" --algo ppo --clip-param 0.2 --use-gae --log-dir "logs_gridworld/Diagonal/PPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 250000 --use-linear-lr-decay --use-proper-time-limits
 
-    python test.py --env-name "Diagonal-v1" --algo ppo --alpha 0.01 --clip-param 0.2 --use-gae --log-dir "logs_gridworld/Diagonal/SPPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 250000 --use-linear-lr-decay --use-proper-time-limits --augment-type "shifted" --temperature-decay-rate 0.93  
+    python test.py --env-name "Diagonal-v1" --algo ppo --alpha 0.01 --clip-param 0.2 --use-gae --log-dir "logs_gridworld/Diagonal/SPPO-${i}" --seed $i --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --gae-lambda 0.95 --num-env-steps 250000 --use-linear-lr-decay --use-proper-time-limits --augment-type "shifted" --temperature-decay-rate 0.93  --epochs-drop 10
 
 done
 
